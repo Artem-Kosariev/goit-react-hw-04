b207544a6e32abdceaf92780c649364148cd20a7
Artem Kosariev <rkosyxa@gmail.com>
2024-11-26 10:07:34 +0500

    1

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+interface Photo {
+  id: string;
+  alt_description: string;
+  urls: {
+    small: string,
+    full: string,
+    regular: string,
+  };
+  user: {
+    name: string,
+    profile_image: {
+      small: string,
+    },
+  };
+}
+
+interface ApiResponse {
+  total: number;
+  total_pages: number;
+  results: Photo[];
+}
+
+interface Image {
+  id: string;
+  alt_description: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+}
+
+interface FormValues {
+  query: string;
+}
+
+interface AppState {
+  page: number;
+  queryValue: string;
+  gallery: Image[];
+  isLoading: boolean;
+  isError: boolean;
+  totalPages: number;
+  noResults: boolean;
+  modalOpen: boolean;
+  modalImage: string;
+  alt: string;
+}
